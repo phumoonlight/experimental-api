5f9ee61a7a4e9716af8ada5db4201d8c34f013a0
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-24 15:06:07 +0700

    fix export file

--- src/features/tags/index.js
+++ src/features/tags/index.js
@@ -1,3 +1,2 @@
 export { default as TagRouter } from './tags-router'
-export { default as TagModel } from './tags-model'
-export { default as TagDataModel } from './tags-data-model'
+export { TagModel } from './tags-model'
