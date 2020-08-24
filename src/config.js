40794bbd869a45f0aeb33e05cb182d9e785f3ef8
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-24 13:24:46 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-import { config } from 'dotenv'
+import dotenv from 'dotenv'
 
-config()
-export const DEV_URL = 'http://localhost:8080/'
-export const PROD_URL = 'https://experimentalapi.herokuapp.com/'
-export const GITHUB_REPO_URL = 'https://github.com/phumoonlight/experimental-api'
+dotenv.config()
 
 export const {
   PORT = 8080,
@@ -11,6 +8,9 @@ export const {
   MONGO_CONNECTION,
 } = process.env
 
+export const DEV_URL = 'http://localhost:8080/'
+export const PROD_URL = 'https://experimentalapi.herokuapp.com/'
+export const GITHUB_REPO_URL = 'https://github.com/phumoonlight/experimental-api'
 export const SERVER_URL = NODE_ENV === 'development' ? DEV_URL : PROD_URL
 
 export const MONGO_COLLECTIONS = {
