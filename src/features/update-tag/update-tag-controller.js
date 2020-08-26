7d18b66490002dd82b68842dce6f4c05cd365e26
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-26 16:54:35 +0700

    refactor

--- src/features/update-tag/update-tag-controller.js
+++ src/features/update-tag/update-tag-controller.js
@@ -3,24 +3,21 @@ import { TagModel } from '../../models'
 export const updateTag = async (req, res) => {
   const { id } = req.params
   const { tagName, tagDescription } = req.body
-  const response = {
-    status: 200,
-    body: {},
-  }
-
+  const responseBody = {}
+  let responseStatus = 200
   try {
     const tagDocument = await TagModel.findOne({ _id: id })
     if (tagName) tagDocument.tag_name = tagName
     if (tagDescription) tagDocument.tag_description = tagDescription
     const updateResult = await tagDocument.save()
-    response.status = 200
-    response.body.updated_result = updateResult
+    responseStatus = 200
+    responseBody.updated_result = updateResult
   } catch (error) {
-    response.status = 500
-    response.body.error = error
+    responseStatus = 500
+    responseBody.error = error
   }
 
-  res.status(response.status).send(response.body)
+  res.status(responseStatus).json(responseBody)
 }
 
 export default updateTag
