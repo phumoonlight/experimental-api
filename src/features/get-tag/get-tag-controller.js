61a847f0e9b0a5e1cb06ccc1a75fea21eeb22ea3
Phusan Moonmuang <phumoonlight@hotmail.com>
2020-08-30 11:53:09 +0700

    Update get-tag-controller.js

--- src/features/get-tag/get-tag-controller.js
+++ src/features/get-tag/get-tag-controller.js
@@ -1,10 +1,12 @@
 import { TagModel } from '../../models'
 
+const SELECTED_FIELD = '-__v'
+
 export const getTags = async (req, res) => {
   const responseBody = {}
   let responseStatus = 200
   try {
-    const tags = await TagModel.find()
+    const tags = await TagModel.find().select(SELECTED_FIELD)
     responseBody.tags = tags
   } catch (error) {
     responseBody.error = error
